8e8f33aae75f9de9a28a74d673d65468d1e97e58
Natalie Enkin <natalie@enkin.co.il>
2021-04-19 13:50:16 +0300

    empty react app using cdn

---
+++ script.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Initial React app</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
